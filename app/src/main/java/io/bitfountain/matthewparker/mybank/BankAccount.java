cc95c47a87b873714102366d79e5ecea0beaa4f1
Matt Parker <mdparker89@gmail.com>
2014-12-07 15:17:27 -0500

    add balance and getter

--- app/src/main/java/io/bitfountain/matthewparker/mybank/BankAccount.java
+++ app/src/main/java/io/bitfountain/matthewparker/mybank/BankAccount.java
@@ -4,11 +4,17 @@ package io.bitfountain.matthewparker.mybank;
  * Created by matthewparker on 12/7/14.
  */
 public class BankAccount {
+    private double balance;
+
     public void withdraw(double amount){
 
     }
 
     public void deposit(double amount){
-        
+
+    }
+
+    public double getBalance(){
+        return balance;
     }
 }
