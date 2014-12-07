c39708f5f642a05a1cd3ed331fc655f4967c180f
Matt Parker <mdparker89@gmail.com>
2014-12-07 12:19:10 -0500

    add bankaccount

---
+++ app/src/main/java/io/bitfountain/matthewparker/mybank/BankAccount.java
@@ -0,0 +1,14 @@
+package io.bitfountain.matthewparker.mybank;
+
+/**
+ * Created by matthewparker on 12/7/14.
+ */
+public class BankAccount {
+    public void withdraw(double amount){
+
+    }
+
+    public void deposit(double amount){
+        
+    }
+}
