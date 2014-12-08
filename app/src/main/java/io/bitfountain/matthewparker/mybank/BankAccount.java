eb84ee6131721c190503eea7bb4a3be51f70be3f
Matt Parker <mdparker89@gmail.com>
2014-12-08 07:18:48 -0500

    charge overdraft fee

--- app/src/main/java/io/bitfountain/matthewparker/mybank/BankAccount.java
+++ app/src/main/java/io/bitfountain/matthewparker/mybank/BankAccount.java
@@ -1,14 +1,21 @@
 package io.bitfountain.matthewparker.mybank;
 
+import android.util.Log;
+
 /**
  * Created by matthewparker on 12/7/14.
  */
 public class BankAccount {
+    private static final String TAG = "BankAccount";
     private double mBalance;
     public static final double OVERDRAFT_FEE = 30;
 
     public void withdraw(double amount){
         mBalance -= amount;
+
+        if(mBalance < 0){
+            mBalance -= OVERDRAFT_FEE;
+        }
     }
 
     public void deposit(double amount){
